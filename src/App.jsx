bb75aefcc6888c526aff53241fde45e8e92f8eb3
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-03-05 20:45:14 +0500

    workedOnLazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+import { Suspense,lazy } from "react";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import SetAvatar from "./pages/SetAvatar";
+import loader from "./assets/loader.gif";
+import styled from "styled-components";
+
+const Loader =()=>{
+  return (
+    <Container>
+
+      <img src={loader} alt="loaderLazy"/>
+    </Container>
+  )
+}
+
+const Loadable =(Component)=>(props)=>{
+
+  return (
+    <Suspense fallback={<Loader/>}>
+<Component {...props}/>
+    </Suspense>
+  )
+}
 function App() {
   return (
     <BrowserRouter>
@@ -17,3 +37,17 @@ function App() {
 }
 
 export default App;
+
+const   Register = Loadable(lazy(()=>import ("./pages/Register")));
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+  `
